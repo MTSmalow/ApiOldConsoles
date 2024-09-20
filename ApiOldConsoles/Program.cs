0134171d38ed45f867a665bfd7cf22844b79a594
MTSmalow <eduardo.diniz4@etec.sp.gov.br>
2024-09-20 09:40:29 -0300

    migração automatica

--- ApiOldConsoles/Program.cs
+++ ApiOldConsoles/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ApiOldConsoles.Data;
+using ApiOldConsoles.Models;
+using Microsoft.CodeAnalysis.CSharp;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<OldConContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("OldConContext") ?? throw new InvalidOperationException("Connection string 'OldConContext' not found.")));
@@ -13,6 +16,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<OldConContext>();
+    context.Database.Migrate();
+
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
